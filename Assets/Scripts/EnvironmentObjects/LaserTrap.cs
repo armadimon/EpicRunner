8dadec1a1b65d1320d58547970a7f3f50ffdfc62
Gori <wnsrjf0922@gmail.com>
2025-03-10 23:43:51 +0900

    레이저 트랩 완성

--- Assets/Scripts/EnvironmentObjects/LaserTrap.cs
+++ Assets/Scripts/EnvironmentObjects/LaserTrap.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class LaserTrap : MonoBehaviour
 {
-    public bool isLeft;
-    public float rayDistance;
+    public Transform firePoint; // 레이저를 발사할 위치
+    public float range = 10f;   // 레이저 감지 거리
+    public LayerMask targetLayer; // 감지할 대상 레이어
+    public SpikeTrap[] spikeTraps;
+    public bool isLeft = true;
+
     void Update()
     {
-        
+        ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(firePoint.position, isLeft == true ? -(firePoint.right) : firePoint.right, out hit, range, targetLayer))
+        {
+            for (int i = 0; i < spikeTraps.Length; i++)
+            {
+                spikeTraps[i].PopUp();
+            }
+        }
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(firePoint.position, firePoint.position + firePoint.right * range);
     }
 }
